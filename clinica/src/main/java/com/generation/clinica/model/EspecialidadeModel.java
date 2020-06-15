8a40a9c62d62d1d8f253f922c8f3042cb54726d4
jonatasdamascenoalves <61021433+jonatasdamascenoalves@users.noreply.github.com>
2020-06-15 19:11:46 -0300

    aula 15/06/2020

---
+++ clinica/src/main/java/com/generation/clinica/model/EspecialidadeModel.java
@@ -0,0 +1,40 @@
+package com.generation.clinica.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="tbEspecialidade")
+public class EspecialidadeModel {
+
+	@Id //PRIMARY KEY
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@JsonIgnoreProperties("codEspecialidade")
+	private long codEspecialidade;
+	
+	@Column(name="nomeEspecialidade", nullable=false, length=50)
+	private String  nomeEspecialidade;
+
+	public long getCodEspecialidade() {
+		return codEspecialidade;
+	}
+
+	public void setCodEspecialidade(long codEspecialidade) {
+		this.codEspecialidade = codEspecialidade;
+	}
+
+	public String getNomeEspecialidade() {
+		return nomeEspecialidade;
+	}
+
+	public void setNomeEspecialidade(String nomeEspecialidade) {
+		this.nomeEspecialidade = nomeEspecialidade;
+	}
+	
+}
